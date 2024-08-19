ea94d95c00c738b7746c1fdb10bfe61d48bb6731
jlneuser <jlneuser@outlook.com>
2024-08-19 15:27:25 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+var input = 'whale phrase for this project';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+var resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+      }
+  }
+
+}
+var resultString = resultArray.join('').toUpperCase();
+
+console.log(resultString);
